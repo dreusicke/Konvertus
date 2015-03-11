f9e4a929e75b22b5ef5e73e9b462d1fc44a78d8c
dreusicke <michaeldreusicke@MacBook-Pro.fritz.box>
2015-03-11 18:52:06 +0100

    date display simplified

--- other files/Description Default To Client.js
+++ other files/Description Default To Client.js
@@ -3,14 +3,12 @@
 14 days after Order Date.
 
 <!-- @@Formula: 
-  if      (issue.get("customfield_10647") != null && issue.get("customfield_10614") == null) {
+  if      (issue.get("customfield_10647") != null ) {
   	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_10647"),14);
-  }
-  else if (issue.get("customfield_10647") == null && issue.get("customfield_10614") != null) {
-  	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_10614"),14);
   }
   else { return null; }
 -->
+
 
 /* <script type="text/javascript">
   planned = document.getElementById('customfield_10015');
---
+++ other files/Description Ordered.js
@@ -0,0 +1,11 @@
+Client ordered or quick-ordered the conversion.
+<!-- @TransitionId: 551,651 -->
+
+<!-- @@Format:
+  if      (issue.get("customfield_10647") != null && issue.get("customfield_10614") == null) {
+	  return "<img src='http://icons.iconarchive.com/icons/sykonist/south-park/32/Cartman-normal-icon.png'> "
+  }
+  else if (issue.get("customfield_10647") == null && issue.get("customfield_10614") != null) {
+	  return "<img src='http://icons.iconarchive.com/icons/sykonist/south-park/32/Cartman-normal-icon.png'> "
+  }
+-->
