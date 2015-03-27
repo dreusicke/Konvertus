7b2b606092ed99c285ea95d64512e022e1c36c51
dreusicke <michaeldreusicke@MacBook-Pro.fritz.box>
2015-03-27 09:34:36 +0100

    added

---
+++ other files/Description Time for Production_02.js
@@ -0,0 +1,10 @@
+Remaining time for production until 'KP to KM'.
+
+<!-- @@Formula: 
+  if      (issue.get("customfield_10900") != null {
+  	return  issue.get("customfield_10900") - new Date();
+  }
+  else { return null; }
+
+
+-->
