7620c2c3016006c719a0f594dd5f41bf773d4d90
dreusicke <michaeldreusicke@MacBook-Pro.local>
2015-04-08 19:25:33 +0200

    new try, also fails

--- other files/Description Time for Production_02.js
+++ other files/Description Time for Production_02.js
@@ -2,7 +2,7 @@ Remaining time for production until 'KP to KM'.
 
 <!-- @@Formula: 
   if      (issue.get("customfield_10900") != null {
-  	return  issue.get("customfield_10900") - new Date();
+  	return  issue.get("customfield_10900") - new Date().getTime();
   }
   else { return null; }
 
