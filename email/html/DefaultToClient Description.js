41189e16e2b4af4926f5bdb5a6c537462f9ee176
dreusicke <michaeldreusicke@MacBook-Pro.local>
2015-06-05 16:38:35 +0200

    solution from Alexey (Polontech)

--- email/html/DefaultToClient Description.js
+++ email/html/DefaultToClient Description.js
@@ -1,8 +1,8 @@
 7 days after Order Date.
 
 <!-- @@Formula: 
-/* if 'Ordered' is not Null and 'Planed to Client' is Null and '' is not 'simple' */
-  if      (issue.get("customfield_10647") != null && issue.get("customfield_10015") == null) {
+/* if 'Ordered' is not Null and 'Planed to Client' is Null and 'Complexity of the Title' is not 'simple' */
+  if      (issue.get("customfield_10647") != null && issue.get("customfield_10015") == null && issue.get("customfield_10005") != 10003) {
   	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_10647"),7);
   }
   else { return null; }
