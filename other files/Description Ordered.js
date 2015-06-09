edf32f5a42de184c9ad18369730cb40be255a460
dreusicke <michaeldreusicke@MacBook-Pro.local>
2015-06-09 18:55:02 +0200

    new transitions added

--- other files/Description Ordered.js
+++ other files/Description Ordered.js
@@ -1,12 +1,3 @@
-Client ordered or quick-ordered the conversion.
+Client ordered with quotation.
 
-<!-- @TransitionId: 551,651,771 -->
-
-<!-- @@Format:
-  if      (issue.get("customfield_10647") != null && issue.get("customfield_10614") == null) {
-	  return "<img src='http://icons.iconarchive.com/icons/sykonist/south-park/32/Cartman-normal-icon.png'> "
-  }
-  else if (issue.get("customfield_10647") == null && issue.get("customfield_10614") != null) {
-	  return "<img src='http://icons.iconarchive.com/icons/sykonist/south-park/32/Cartman-normal-icon.png'> "
-  }
--->
+<!-- @TransitionId: 551,801,881 -->
