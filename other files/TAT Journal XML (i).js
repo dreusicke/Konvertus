bcafdc50f43667dec566129640817319c57867dd
dreusicke <michaeldreusicke@MacBook-Pro-3.local>
2016-04-04 19:30:26 +0530

    events for XML last workflow added

--- other files/TAT Journal XML (i).js
+++ other files/TAT Journal XML (i).js
@@ -1,11 +1,14 @@
 5 calendar days after 'production ordered'.
 
-
 <!-- @@Formula: 
 
-/* If '1st Proof Production Started' is Not Null, add 5 days */
+/* If '1st Proof Production Started' is Not Null, add 7 days */
   if (issue.get("customfield_13018") != null) {
   	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_13018"),5);
+  }
+/* If '1st Proof Production Started' is Null and 'Production Ordered' is NOT Null, add 7 days */
+  else if (issue.get("customfield_13018") == null && issue.get("customfield_12506") != null) {
+  	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_12506"),5);
   }
   else { return null; }
 -->
--- other files/TAT Journal XML.js
+++ other files/TAT Journal XML.js
@@ -1,11 +1,14 @@
 7 calendar days after 'production ordered'.
 
-
 <!-- @@Formula: 
 
 /* If '1st Proof Production Started' is Not Null, add 7 days */
   if (issue.get("customfield_13018") != null) {
   	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_13018"),7);
+  }
+/* If '1st Proof Production Started' is Null and 'Production Ordered' is NOT Null, add 7 days */
+  else if (issue.get("customfield_13018") == null && issue.get("customfield_12506") != null) {
+  	return  org.apache.commons.lang.time.DateUtils.addDays(issue.get("customfield_12506"),7);
   }
   else { return null; }
 -->
