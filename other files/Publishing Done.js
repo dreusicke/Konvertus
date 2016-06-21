58dfed5a7c11891a7c05f483a3cd81d0d76f636e
dreusicke <michaeldreusicke@MacBook-Pro-3.local>
2016-06-21 15:36:49 +0530

    Heavy bug in formula
    JIRA was nearly down, this took many resources. The field is empty
    right now.

--- other files/Publishing Done.js
+++ other files/Publishing Done.js
@@ -16,3 +16,29 @@ if  (issue.get("customfield_12550") != null ) {
 -->
 
 <!-- @@Format: COMPLETE -->
+
+
+
+**** **** **** ****
+
+KOPIERT AUS JIRA IST DIESE FELDBESCHREIBUNG, DIE MACHT ABER VIELE PROBLEME
+
+WdG
+'Dispatch Done' date plus the time for 'Publishing', by default 2 days.
+
+<!-- @@Formula: 
+
+/* if 'Days for Publishing' is not Null, return 'Dispatch Done' + 'Days for Publishing' */
+
+
+  if  ((int)issue.get("customfield_12617") > (int)issue.get("customfield_12558") ) {
+
+      return  issue.get("customfield_12617");
+    
+}
+
+-->
+
+<!-- @@Format: COMPLETE -->
+
+**** **** **** ****
